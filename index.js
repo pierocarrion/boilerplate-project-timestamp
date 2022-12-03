e256eb52de25c80f18d291900596419685cca785
pierocarrion <piero.january15@gmail.com>
2022-12-03 18:51:14 -0500

    Challenges Completed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +21,27 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/:date?", function (req, res) {
+  const date = req.params?.date;
+  const dateRegex = /\d{4}[-]\d{2}[-]\d{2}|\s/;   //Date regex
+  const numberRegex = /\d{10}/;                   //Number regex
 
+  if (!date) {
+    res.json({ unix: new Date().getTime(), utc: new Date().toUTCString() });
+  }
+
+  if (dateRegex.test(date)) {
+    res.json({ unix: new Date(date).getTime(), utc: new Date(date).toUTCString() });
+  } else if (numberRegex.test(date)) {
+    res.json({ unix: +date, utc: new Date(+date).toUTCString() });
+  } else {
+    res.json({ error: 'Invalid Date' });
+  };
+  
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
